aa8a17f8b0c55f5cb9ee0b0add7ef048e9cdab37
julkarnain <julkar.elixirsoft.nain@gmail.com>
2022-09-30 20:44:33 +0600

    Auto code is generated by STS

---
+++ src/main/java/com/elixirsoft/feature/Calculator.java
@@ -0,0 +1,10 @@
+package com.elixirsoft.feature;
+
+/**
+ * Simple calculator interface.
+ */
+public interface Calculator {
+    int add(int x, int y);
+    int multiply(int x, int y);
+    void save(int x);
+}
